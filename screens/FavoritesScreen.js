9f22ad2815bb6d233d786af3385d1c850680f2d3
suesanz <yadavsourav24071998@gmail.com>
2019-10-20 06:12:01 +0530

    first commit

---
+++ screens/FavoritesScreen.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import {HeaderButtons, Item} from 'react-navigation-header-buttons';
+import {useSelector} from 'react-redux';
+import DefaultText from '../components/DefaultText'
+import HeaderButton from '../components/HeaderButton';
+import MealList from '../components/MealList';
+import {View, StyleSheet} from "react-native";
+
+const FavoritesScreen = props => {
+    const favMeals = useSelector(state => state.meals.favoriteMeals);
+    if (favMeals.length === 0 || !favMeals) {
+        return <View style={styles.content}><DefaultText>No Favorites..!! Starting adding one.</DefaultText></View>
+    }
+    return <MealList listData={favMeals} navigation={props.navigation}/>;
+};
+
+FavoritesScreen.navigationOptions = navData => {
+    return {
+        headerTitle: 'Your Favorites',
+        headerLeft: (
+            <HeaderButtons HeaderButtonComponent={HeaderButton}>
+                <Item
+                    title="Menu"
+                    iconName="ios-menu"
+                    onPress={() => {
+                        navData.navigation.toggleDrawer();
+                    }}
+                />
+            </HeaderButtons>
+        )
+    };
+};
+const styles = StyleSheet.create({
+    content: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center'
+    }
+})
+export default FavoritesScreen;
